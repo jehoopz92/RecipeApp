0d9c53fe30cf8e1a36a5a671aa3e17362df9ebcd
Jonathan Emery <jonathane@carsons.local>
2019-08-19 11:46:00 -0400

    Updates

---
+++ precache-manifest.89faff85f15c60a303fca228875baf52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcef334dbb30874418c735ef27544f17",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "06add4b1136a07220245",
+    "url": "/RecipeApp/static/css/main.003fddff.chunk.css"
+  },
+  {
+    "revision": "f86ae8f3fbe2703fc4fa",
+    "url": "/RecipeApp/static/js/2.562f5c5c.chunk.js"
+  },
+  {
+    "revision": "06add4b1136a07220245",
+    "url": "/RecipeApp/static/js/main.55982a7c.chunk.js"
+  },
+  {
+    "revision": "d50c7ce8dfad8c3c2e1d",
+    "url": "/RecipeApp/static/js/runtime~main.93662b29.js"
+  },
+  {
+    "revision": "b58b713b166bdcfb99cfbdf23ad1f207",
+    "url": "/RecipeApp/static/media/spinner.b58b713b.gif"
+  }
+]);
