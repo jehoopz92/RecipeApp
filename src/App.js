6cc9357ed186050ebfe18714e16abe67c9ac78d9
Jonathan Emery <jonathane@carsons.local>
2020-05-13 10:18:11 -0400

    Final Project

--- src/App.js
+++ src/App.js
@@ -8,33 +8,15 @@ import './App.css';
 import Index from './components/layout/Index';
 
 class App extends Component {
-  // const UrlGet = '4cd34c584bf1c6c25f36ec726fad9372'
-
-  // const [recipes, setRecipes] = useState([]);
-  // const [search, setSearch] = useState('');
-  // const [query, setQuery] = useState('chicken')
-
-  // useEffect(() => {
-  //   getRecipes()
-  // }, [query])
-
-  // const updateSearch = e => {
-  //   setSearch(e.target.value);
-  // }
-
-  // const getSearch = e => {
-  //   e.preventDefault();
-  //   setQuery(search)
-  //   setSearch('')
-  // }
   render() {
+    console.log('Production App 4.0');
     return (
       <Provider>
         <Router>
           <React.Fragment>
             <div className="App">
               <Switch>
-                <Route exact path="/" component={Index} />
+                <Route exact path="" component={Index} />
               </Switch>
             </div>
           </React.Fragment>
@@ -45,13 +27,3 @@ class App extends Component {
 }
 
 export default App;
-
-/* <form className="search-form" onSubmit={getSearch}>
-        <input type="text" className="search-bar"value={search} onChange={updateSearch}/>
-        <button type="submit" className="search-button">Submit</button>
-      </form>
-      <div className="recipes">
-      {recipes.map(recipe => (
-        <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients}/>
-      ))}
-      </div> */
